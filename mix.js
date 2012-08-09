8f2ed9de63c82dfcb166eb8fea163586ecf82fa1
i.mineev <i.mineev@mineev.inostudio.net>
2012-08-09 19:23:03 +0400

    Убран селектор $ из MIX

--- mix.js
+++ mix.js
@@ -27,21 +27,6 @@
             }
             return current;
         },
-        $: function (selector){
-            var action = selector[0],
-            el = false;
-            switch(action){
-                case '#' :
-                    el = document.getElementById(selector.substr(1))
-                    break
-                case '.' :
-                    el = document.getElementsByClass(selector.substr(1))
-                    break
-                default:
-                    el = document.getElementsByTagName(selector)
-            }
-            return el;
-        },
         apply: function (o, c, defaults){
             // no "this" reference for friendly out of scope calls
             if (defaults) {
@@ -393,36 +378,7 @@
             return _function.apply(scope, arguments);
         }
     }
-    //getElementsByClassName
-    if(document.getElementsByClassName) {
 
-        getElementsByClass = function(classList, node) {    
-            return (node || document).getElementsByClassName(classList)
-        }
-
-    } else {
-
-        getElementsByClass = function(classList, node) {			
-            var node = node || document,
-            list = node.getElementsByTagName('*'), 
-            length = list.length,  
-            classArray = classList.split(/\s+/), 
-            classes = classArray.length, 
-            result = [], i,j
-            for(i = 0; i < length; i++) {
-                for(j = 0; j < classes; j++)  {
-                    if(list[i].className.search('\\b' + classArray[j] + '\\b') != -1) {
-                        result.push(list[i])
-                        break
-                    }
-                }
-            }
-	
-            return result
-        }
-    }
-
-  
 })();
 
 
